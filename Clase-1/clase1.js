ee486cb5ca8c9c134abae689b0ec408a19c8b87c
JosueOb <josuericardocando@hotmail.com>
2019-02-05 17:03:01 -0500

    Introducción a variables JS

---
+++ Clase-1/clase1.js
@@ -0,0 +1,9 @@
+// Variables
+// var nombre = 'Josué'
+// var apellido = 'Cando'
+var nombre = 'Josué', apellido = 'Cando'
+var edad = 28
+edad = '28'
+
+console.log('Hola ' + nombre + ' ' + apellido)
+console.log('Tengo '+ edad + ' años')
