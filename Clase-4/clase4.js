91a1ee26c530a29859cfd6ca42c6aa42b76a94a6
JosueOb <josuericardocando@hotmail.com>
2019-02-06 17:55:33 -0500

    Clase 4 - funciones

---
+++ Clase-4/clase4.js
@@ -0,0 +1,12 @@
+var nombre = 'Josué', edad = 28
+
+//Función con parametros
+function imprimirEdad(n, e){
+    console.log(`${n} tiene ${e} anios`)
+}
+imprimirEdad(nombre,edad)
+imprimirEdad('Karol',22)
+imprimirEdad('Maria',20)
+imprimirEdad(18,'Ricardo')
+imprimirEdad('Alexa')
+imprimirEdad()
